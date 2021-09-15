b9dec9a3ba81b0f9009187ab6a279da043e6e2b3
julienperce <58319391+julienperce@users.noreply.github.com>
2021-09-14 23:04:27 -0500

    create LFC desc, update js function to work for all descs

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function showDesc(stop) {
-    eltCnt = document.getElementsByClassName("lfcDescWindow").length;// elt count per class
+    eltCnt = document.getElementsByClassName(stop).length;// elt count per class
     for (i = 0; i < eltCnt; i++)
     {
-        document.getElementsByClassName("lfcDescWindow")[i].style.visibility = "visible";
+        document.getElementsByClassName(stop)[i].style.visibility = "visible";
     }
 }
 
 function hideDesc(stop) {
-    eltCnt = document.getElementsByClassName("lfcDescWindow").length;// elt count per class
+    eltCnt = document.getElementsByClassName(stop).length;// elt count per class
     for (i = 0; i < eltCnt; i++)
     {
-        document.getElementsByClassName("lfcDescWindow")[i].style.visibility = "hidden";
+        document.getElementsByClassName(stop)[i].style.visibility = "hidden";
     }
 }
