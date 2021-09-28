4ea8f3746d7e3f1b028a5b1c8fbc63daf4c3be6a
julienperce <58319391+julienperce@users.noreply.github.com>
2021-09-27 23:00:04 -0500

    aboutme window done, some js updates

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ function hideDesc(stop) {
         document.getElementsByClassName(stop)[i].style.visibility = "hidden";
     }
 }
+
+function giveAge() {
+    var born = new Date(2004, 04, 06, 0, 30, 0, 0);
+    var day = new Date();
+
+    var dif = day.getTime() - born.getTime();
+
+    var bornSeconds = dif / 1000;
+    var absSeconds = Math.abs(bornSeconds);
+    var absHours = absSeconds / 3600;
+    var absDays = absSeconds / 24;
+    var absYears = absSeconds / 31556952;
+    return absYears;
+}
+
+function writeAge() {
+    years = giveAge();
+    document.getElementById("ageyears").innerHTML = `- ${years} years old`;
+    setTimeout(writeAge, 100)
+}
